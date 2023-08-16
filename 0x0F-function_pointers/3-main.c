4fc02617c4121fc23fc8177058557e71e549e0ec
Ndivhuwo <Cyprian2214@gmail.com>
2023-08-16 07:58:40 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point of the program.
+ * @argc: Number of arguments.
+ * @argv: Array of pointers to the arguments.
+ *
+ * Return: 0 (Success).
+ */
+int main(int argc, char *argv[])
+{
+    int num1, num2, result;
+    int (*func_ptr)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        return (98);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+
+    func_ptr = get_op_func(argv[2]);
+    if (func_ptr == NULL)
+    {
+        printf("Error\n");
+        return (99);
+    }
+
+    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        return (100);
+    }
+
+    result = func_ptr(num1, num2);
+    printf("%d\n", result);
+
+    return (0);
+}
