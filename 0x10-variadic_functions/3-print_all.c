afeeb38990e76cf01065bcf9aecd7d774683eeca
Ndivhuwo <Cyprian2214@gmail.com>
2023-08-17 05:00:46 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,14 +3,8 @@
 #include <stdio.h>
 
 /**
- * This function prints a variable number of arguments based on the provided format string.
- * It supports printing characters, integers, floats, and strings.
- *
- * @param format A string specifying the format of the arguments to be printed.
- *               Supported format specifiers: 'c' for character, 'i' for integer,
- *               'f' for float, 's' for string.
- * @param ...    The list of arguments corresponding to the format specifiers.
- *               The number and type of arguments should match the format string.
+  * print_all - prints anything.
+ * @format: list of types of arguments passed to the function.
  */
 void print_all(const char * const format, ...)
 {
