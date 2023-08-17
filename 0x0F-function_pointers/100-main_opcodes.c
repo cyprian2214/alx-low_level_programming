54a3c07d29528c7b4ca54125d97d0acfd44440aa
Ndivhuwo <Cyprian2214@gmail.com>
2023-08-17 04:14:43 -0700

    Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_opcodes(int num_bytes) {
-    unsigned char *main_ptr = (unsigned char *)print_opcodes;
-    int i;
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *arr;
 
-    for (i = 0; i < num_bytes; i++) {
-        printf("%02x", main_ptr[i]);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    printf("\n");
-}
+	bytes = atoi(argv[1]);
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    int num_bytes = atoi(argv[1]);
+	arr = (char *)main;
 
-    if (num_bytes < 0) {
-        printf("Error\n");
-        return 2;
-    }
-
-    print_opcodes(num_bytes);
-
-    return 0;
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
