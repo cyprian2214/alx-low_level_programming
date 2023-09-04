1b91b188d0bc36510c492fb98f220ec35b2fa79b
Ndivhuwo <Cyprian2214@gmail.com>
2023-09-04 12:10:23 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,69 +1,65 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <elf.h>
-#include <string.h>
-#include <errno.h>
 
-/**
- * ELF Header Information Retrieval Program
- *
- * This program reads and displays information from the ELF header of an ELF file.
- * It displays information such as the ELF magic number, class, data format, version,
- * OS/ABI, ABI version, file type, and entry point address.
- */
+#define BUFSIZE 64
 
-void print_error(const char *message) {
-    fprintf(stderr, "Error: %s\n", message);
-    exit(98);
-}
+void display_elf_header_info(int fd) {
+    Elf64_Ehdr header;
+    int i; /* Declare i outside of the for loop in C90 mode */
 
-void print_elf_header_info(Elf64_Ehdr *header) {
-    unsigned int i;
+    /* Read the ELF header into the 'header' structure */
+    if (read(fd, &header, sizeof(header)) != sizeof(header)) {
+        dprintf(STDERR_FILENO, "Error: Unable to read ELF header\n");
+        exit(98);
+    }
 
-    printf("Magic:   ");
+    /* Check if the file is an ELF file */
+    if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+        header.e_ident[EI_MAG1] != ELFMAG1 ||
+        header.e_ident[EI_MAG2] != ELFMAG2 ||
+        header.e_ident[EI_MAG3] != ELFMAG3) {
+        dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+        exit(98);
+    }
+
+    /* Display the ELF header information */
+    printf("  Magic:   ");
     for (i = 0; i < EI_NIDENT; i++) {
-        printf("%02x ", header->e_ident[i]);
+        printf("%02x ", header.e_ident[i]);
     }
     printf("\n");
-
-    printf("Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-    printf("Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little-endian" : "Unknown data format");
-    printf("Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-    printf("OS/ABI:                            %d\n", header->e_ident[EI_OSABI]);
-    printf("ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-
-    printf("Type:                              %d\n", header->e_type);
-    printf("Entry point address:               0x%lx\n", (unsigned long)header->e_entry);
+    printf("  Class:                             %s\n",
+           header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" :
+           header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "Unknown");
+    printf("  Data:                              %s\n",
+           header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
+           header.e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "Unknown");
+    printf("  Version:                           %d (current)\n", header.e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            %d\n", header.e_ident[EI_OSABI]);
+    printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
+    printf("  Type:                              %d\n", header.e_type);
+    printf("  Entry point address:               %lx\n", header.e_entry);
 }
 
 int main(int argc, char *argv[]) {
-    const char *elf_filename;
-    Elf64_Ehdr header;
-    int fd;
+    int fd; /* Declare fd at the beginning of the function */
 
     if (argc != 2) {
-        print_error("Usage: elf_header elf_filename");
+        dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
+        return 98;
     }
 
-    elf_filename = argv[1];
-    fd = open(elf_filename, O_RDONLY);
+    fd = open(argv[1], O_RDONLY); /* Declare and initialize fd separately */
     if (fd == -1) {
-        print_error(strerror(errno));
+        perror("Error");
+        return 98;
     }
 
-    if (read(fd, &header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr)) {
-        print_error("Failed to read ELF header");
-    }
-
-    if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0) {
-        print_error("Not an ELF file");
-    }
-
-    print_elf_header_info(&header);
-
+    display_elf_header_info(fd);
     close(fd);
     return 0;
 }
